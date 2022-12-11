5e1ada07a4c1edb557c035aa74d138b19cfa5114
rex-xyf <1590926847@qq.com>
2022-12-11 21:33:17 +0800

    amend some bug in delete module

--- meitishangcheng-system/src/main/java/web/Servlet/DeleteServlet/RefuseServlet.java
+++ meitishangcheng-system/src/main/java/web/Servlet/DeleteServlet/RefuseServlet.java
@@ -1,19 +1,26 @@
-package web.Servlet.ScheduleServlet;
+package web.Servlet.DeleteServlet;
+
+import service.DeleteService;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class selectLeaveServlet extends HttpServlet {
+@WebServlet("/refuseServlet")
+public class RefuseServlet extends HttpServlet {
+    DeleteService deleteService = new DeleteService();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        int id = Integer.parseInt(req.getReader().readLine());
+        deleteService.delete(id);
+        resp.getWriter().write("success");
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
+        this.doGet(req, resp);
     }
 }
